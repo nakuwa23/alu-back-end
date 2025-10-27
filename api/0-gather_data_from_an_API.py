92831b2056d5efe0b17aa11bb77701786dc84148
Brian Nakuwa <b.nakuwa@alustudent.com>
2025-10-27 23:08:03 +0000

    Script 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Script that fetches and displays an employee's TODO list progress
+from a REST API given their employee ID.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        sys.exit(1)
+    
+    # Base URL for the API
+    base_url = "https://jsonplaceholder.typicode.com"
+    
+    # Fetch employee data
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    user_response = requests.get(user_url)
+    
+    if user_response.status_code != 200:
+        sys.exit(1)
+    
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+    
+    # Fetch TODO list for the employee
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+    todos_response = requests.get(todos_url)
+    
+    if todos_response.status_code != 200:
+        sys.exit(1)
+    
+    todos_data = todos_response.json()
+    
+    # Calculate progress
+    total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task.get("completed")]
+    number_of_done_tasks = len(completed_tasks)
+    
+    # Display results
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, number_of_done_tasks, total_tasks))
+    
+    # Display completed task titles
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
---
+++ api/README.md
@@ -0,0 +1,1 @@
+Here goes the  intranet API tasks.
