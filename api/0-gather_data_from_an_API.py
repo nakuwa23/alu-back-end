544c6e566137efc98a33d00f8cd5c16b2b7e16e2
Brian Nakuwa <b.nakuwa@alustudent.com>
2025-10-27 23:20:34 +0000

    Script 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,10 +16,8 @@ if __name__ == "__main__":
     except ValueError:
         sys.exit(1)
 
-    # Base URL for the API
     base_url = "https://jsonplaceholder.typicode.com"
 
-    # Fetch employee data
     user_url = "{}/users/{}".format(base_url, employee_id)
     user_response = requests.get(user_url)
 
@@ -29,7 +27,6 @@ if __name__ == "__main__":
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
-    # Fetch TODO list for the employee
     todos_url = "{}/todos?userId={}".format(base_url, employee_id)
     todos_response = requests.get(todos_url)
 
@@ -38,16 +35,12 @@ if __name__ == "__main__":
 
     todos_data = todos_response.json()
 
-    # Calculate progress
     total_tasks = len(todos_data)
     completed_tasks = [task for task in todos_data if task.get("completed")]
     number_of_done_tasks = len(completed_tasks)
 
-    # Display results
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, number_of_done_tasks, total_tasks))
 
-    # Display completed task titles
     for task in completed_tasks:
         print("\t {}".format(task.get("title")))
-        
